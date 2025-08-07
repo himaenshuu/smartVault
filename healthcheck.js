5ea59e00a87001e73969195ec1b74ccba5ab873d
himanshu <118502619+himaenshuu@users.noreply.github.com>
2025-08-07 23:44:56 +0530

    Update healthcheck.js

--- healthcheck.js
+++ healthcheck.js
@@ -1,23 +1,34 @@
-const http = require('http');
+import http from 'http';
 
 const options = {
   host: 'localhost',
   port: process.env.PORT || 3000,
-  timeout: 2000
+  timeout: 2000,
 };
 
-const request = http.request(options, (res) => {
-  console.log(`STATUS: ${res.statusCode}`);
-  if (res.statusCode == 200) {
+const makeRequest = () => {
+  return new Promise((resolve, reject) => {
+    const req = http.request(options, (res) => {
+      console.log(`STATUS: ${res.statusCode}`);
+      res.statusCode === 200 ? resolve() : reject();
+    });
+
+    req.on('error', (err) => {
+      console.error('ERROR:', err.message);
+      reject();
+    });
+
+    req.end();
+  });
+};
+
+const run = async () => {
+  try {
+    await makeRequest();
     process.exit(0);
-  } else {
+  } catch {
     process.exit(1);
   }
-});
+};
 
-request.on('error', function(err) {
-  console.log('ERROR');
-  process.exit(1);
-});
-
-request.end();
+run();
